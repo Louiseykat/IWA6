097e71773001cdb4e426a367c524fa307201c563
Louiseykat <louisekriek90@gmail.com>
2024-02-12 14:55:45 +0200

    completed challenge

---
+++ challenge1.js
@@ -0,0 +1,14 @@
+const primaryPhone = 'O748105141'
+const secondaryPhone = '0219131568'
+
+/* console.log(parseInt(secondaryPhone)) */
+
+// Only change below this line
+
+/* const primaryValid = typeof number == primaryPhone 
+const secondaryValid = typeof number == secondaryPhone
+
+console.log('Primary phone is valid numerical string:', primaryValid)
+console.log('Secondary phone is valid numerical string:', secondaryValid ) */
+
+console.log(typeof secondaryPhone)
